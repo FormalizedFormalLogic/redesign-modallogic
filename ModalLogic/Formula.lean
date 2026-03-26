9fa7c7463ce9790bd3f6a5a2ab31070c1444a8ff
SnO2WMaN <me@sno2wman.net>
2026-03-26 09:06:51 +0900

    Kripke semantics

--- ModalLogic/Formula.lean
+++ ModalLogic/Formula.lean
@@ -1,6 +1,6 @@
 module
 
-import Mathlib.Data.Vector.Basic
+public import Mathlib.Data.Vector.Basic
 
 @[expose] public section
 
@@ -11,45 +11,57 @@ inductive Formula (τ : Similarity) (α : Type*)
 | atom   : α → Formula τ α
 | falsum : Formula τ α
 | imply  : Formula τ α → Formula τ α → Formula τ α
-| dia    : {arity : Nat} → τ arity → (Fin arity → Formula τ α) → Formula τ α
+| dia'   : {arity : Nat} → τ arity → (Fin arity → Formula τ α) → Formula τ α
 
 variable {τ : Similarity} {α : Type*}
 
+abbrev FormulaSeq (τ α arity) := Vector (Formula τ α) arity
+
 namespace Formula
 
+
 prefix:75 "#" => atom
-infixr:67 " → " => imply
-notation:91 "▽[" t "]" Φ => dia t Φ
+infixr:67 " 🡒 " => imply
+
+abbrev dia {arity : Nat} (t : τ arity) (Φ : FormulaSeq τ α arity) : Formula τ α := dia' t Φ.get
+notation:91 "△[" t "]" Φ => dia t Φ
+
 notation:max "⊥" => falsum
 
-abbrev neg (φ : Formula τ α) : Formula τ α := φ → ⊥
+abbrev neg (φ : Formula τ α) : Formula τ α := φ 🡒 ⊥
 prefix:85 "∼" => neg
 
-abbrev or (φ ψ : Formula τ α) : Formula τ α := ∼φ → ψ
+abbrev or (φ ψ : Formula τ α) : Formula τ α := ∼φ 🡒 ψ
 infixr:68 " ⋎ " => or
 
-abbrev and (φ ψ : Formula τ α) : Formula τ α := ∼(φ → ∼ψ)
+abbrev and (φ ψ : Formula τ α) : Formula τ α := ∼(φ 🡒 ∼ψ)
 infixr:69 " ⋏ " => and
 
 abbrev verum : Formula τ α := ∼⊥
 notation:max "⊤" => ∼⊥
 
+abbrev iff (φ ψ : Formula τ α) : Formula τ α := (φ 🡒 ψ) ⋏ (ψ 🡒 φ)
+notation:70 " 🡘 " => iff
+
 end Formula
 
 
-abbrev FormulaSeq (ar : ℕ) (τ) (α) := Fin ar → Formula τ α
 namespace FormulaSeq
 
-abbrev neg (Φ : FormulaSeq ar τ α) : FormulaSeq ar τ α := λ i => ∼(Φ i)
+abbrev neg (Φ : FormulaSeq τ α arity) : FormulaSeq τ α arity := Φ.map Formula.neg
 prefix:85 "∼" => neg
+
+variable {Φ : FormulaSeq τ α arity} {i : Fin arity}
+
+@[grind =] lemma eq_neg_get_get_neg : (∼Φ).get i = ∼(Φ.get i) := by apply Vector.getElem_map
 
 end FormulaSeq
 
 
 namespace Formula
 
-abbrev box {arity : ℕ} (t : τ arity) (Φ : FormulaSeq arity τ α) : Formula τ α := ∼(▽[t](∼Φ))
-notation:91 "△[" t "]" Φ => box t Φ
+abbrev box {arity : ℕ} (t : τ arity) (Φ : FormulaSeq τ α arity) : Formula τ α := ∼(△[t](∼Φ))
+notation:91 "▽[" t "]" Φ => box t Φ
 
 end Formula
 
--- ModalLogic/Kripke.lean
+++ ModalLogic/Kripke.lean
@@ -4,9 +4,69 @@ public import ModalLogic.Formula
 
 @[expose] public section
 
-variable {α : Type u}
+variable {τ : Similarity} {α : Type*}
+variable {a : α} {φ ψ : Formula τ α}
 
-structure KripkeFrame (κ : Type*) where
-  acc : κ → κ → Prop
+structure KripkeFrame (τ : Similarity) (κ : Type*) where
+  Rel' : {arity : ℕ} → τ arity → κ → Vector κ arity → Prop
+
+namespace KripkeFrame
+
+variable {F : KripkeFrame τ κ}
+
+abbrev World (_ : KripkeFrame τ κ) := κ
+abbrev Rel {arity : ℕ} (t : τ arity) (w : F.World) (v : Vector F.World arity) : Prop := F.Rel' t w v
+notation:91 w " ≺[" t "] " v => Rel t w v
+
+end KripkeFrame
+
+structure KripkeModel (τ : Similarity) (α : Type*) (κ : Type*) extends KripkeFrame τ κ where
+  val : κ → α → Prop
+
+
+namespace Formula
+
+variable {M : KripkeModel τ α κ} {w : M.World}
+variable {arity} {t : τ arity} {Φ : FormulaSeq τ α arity}
+
+@[grind]
+def KripkeForced {M : KripkeModel τ α κ} (w : M.World) : Formula τ α → Prop
+| .atom a => M.val w a
+| .falsum => False
+| .imply φ ψ => KripkeForced w φ → KripkeForced w ψ
+| .dia' t Φ => ∃ v, (w ≺[t] v) ∧ ∀ i, KripkeForced v[i] (Φ i)
+infixr:25 " ⊩ " => KripkeForced
+
+abbrev NotKripkeForced (w : M.World) (φ : Formula τ α) : Prop := ¬(w ⊩ φ)
+infixr:25 " ⊮ " => NotKripkeForced
+
+lemma kripkeForced_atom   : w ⊩ #a ↔ M.val w a := by rfl
+lemma kripkeForced_falsum : w ⊮ ⊥ := by grind
+lemma kripkeForced_imply  : w ⊩ φ 🡒 ψ ↔ (w ⊩ φ) → (w ⊩ ψ) := by rfl
+lemma kripkeForced_dia    : w ⊩ (△[t]Φ) ↔ ∃ v, (w ≺[t] v) ∧ ∀ i, (v[i]) ⊩ (Φ.get i) := by rfl
+lemma kripkeForced_and    : w ⊩ φ ⋏ ψ ↔ (w ⊩ φ) ∧ (w ⊩ ψ) := by grind
+lemma kripkeForced_or     : w ⊩ φ ⋎ ψ ↔ (w ⊩ φ) ∨ (w ⊩ ψ) := by grind
+lemma kripkeForced_neg    : w ⊩ ∼φ ↔ w ⊮ φ := by grind
+lemma kripkeForced_verum  : w ⊩ ⊤ := by grind
+lemma kripkeForced_box    : w ⊩ (▽[t]Φ) ↔ ∀ v, (w ≺[t] v) → ∃ i, (v[i]) ⊩ (Φ.get i) := by
+    rw [kripkeForced_neg, NotKripkeForced, kripkeForced_dia];
+    grind;
+
+attribute [grind =_]
+    kripkeForced_and
+    kripkeForced_or
+    kripkeForced_neg
+
+attribute [grind .]
+    kripkeForced_verum
+    kripkeForced_falsum
+
+def KripkeModelValid (M : KripkeModel τ α κ) (φ : Formula τ α) : Prop := ∀ w : M.World, w ⊩ φ
+infix:25 "⊧" => KripkeModelValid
+
+def KripkeFrameValid (F : KripkeFrame τ κ) (φ : Formula τ α) : Prop := ∀ V, (⟨F, V⟩ : KripkeModel τ α κ) ⊧ φ
+infix:25 "⊧" => KripkeFrameValid
+
+end Formula
 
 end
